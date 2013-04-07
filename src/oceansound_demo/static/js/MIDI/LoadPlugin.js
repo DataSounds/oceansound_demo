ce1adc5fe282fd88442ca9e1bb23502d86adfb89
Luiz Irber <luiz.irber@gmail.com>
2013-04-07 11:28:21 -0300

    Updated MIDI.js

--- src/oceansound_demo/static/js/MIDI/LoadPlugin.js
+++ src/oceansound_demo/static/js/MIDI/LoadPlugin.js
@@ -27,10 +27,13 @@ MIDI.loadPlugin = function(conf) {
 	/// Get the instrument name.
 	var instruments = conf.instruments || conf.instrument || "acoustic_grand_piano";
 	if (typeof(instruments) !== "object") instruments = [ instruments ];
-	instruments.map(function(data) {
-		if (typeof(data) === "number") data = MIDI.GeneralMIDI.byId[data];
-		return data;
-	});
+	///
+	for (var n = 0; n < instruments.length; n ++) {
+		var instrument = instruments[n];
+		if (typeof(instrument) === "number") {
+			instruments[n] = MIDI.GeneralMIDI.byId[instrument];
+		}
+	};
 	///
 	MIDI.soundfontUrl = conf.soundfontUrl || MIDI.soundfontUrl || "./soundfont/";
 	/// Detect the best type of audio to use.
@@ -72,10 +75,10 @@ connect.flash = function(filetype, instruments, conf) {
 	// fairly quick, but requires loading of individual MP3s (more http requests).
 	if (MIDI.loader) MIDI.loader.message("Flash API...");
 	DOMLoader.script.add({
-		src: "./inc/SoundManager2/script/soundmanager2.js",
+		src: conf.soundManagerUrl || "./inc/SoundManager2/script/soundmanager2.js",
 		verify: "SoundManager",
 		callback: function () {
-			MIDI.Flash.connect(conf);
+			MIDI.Flash.connect(instruments, conf);
 		}
 	});
 };
